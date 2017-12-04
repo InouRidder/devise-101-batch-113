9859c98482b95b59b0578d94b9ece4015954f9f5
Inou Ridder <inouridder@gmail.com>
2017-12-04 10:28:47 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/edit'
-
-  devise_for :registrations, controllers: {registrations: 'registrations'}
+  devise_for :users
   root to: 'pages#home'
   get 'contact', to: 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
